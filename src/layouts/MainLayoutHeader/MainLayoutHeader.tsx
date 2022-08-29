3fe47036647e65b29b21da8fd7182dfe8b7e3a01
Khoa Pham <blackparadise0407@gmail.com>
2022-08-29 15:46:32 +0700

    Add auth0

--- src/layouts/MainLayoutHeader/MainLayoutHeader.tsx
+++ src/layouts/MainLayoutHeader/MainLayoutHeader.tsx
@@ -1,21 +1,70 @@
+import { useUser } from '@auth0/nextjs-auth0';
 import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
 import { RiHeart2Line, RiSearch2Line, RiShoppingBagLine } from 'react-icons/ri';
 
-const StyledLink: React.FC<LinkProps & { children: React.ReactNode }> = (
-  props,
-) => {
-  return <Link {...props}>{props.children}</Link>;
+import { useWindowSize } from '@/hooks/useWindowSize';
+
+const StyledLink: React.FC<
+  LinkProps & {
+    active?: boolean;
+    children: React.ReactNode;
+  }
+> = (props) => {
+  const { children, ...rest } = props;
+  return (
+    <Link {...rest} className="block">
+      <span className="font-medium hover:text-red-400 transition-colors cursor-pointer">
+        {children}
+      </span>
+    </Link>
+  );
 };
 
+const routes: Array<{ href: string; label: string }> = [
+  { href: '/', label: 'Home' },
+  { href: '/shop', label: 'Shop' },
+  { href: '/pages', label: 'Pages' },
+  { href: '/blog', label: 'Blog' },
+  { href: '/contacts', label: 'Contacts' },
+];
+
 const MainLayoutHeader: React.FC<{}> = () => {
+  const { user } = useUser();
+  const { asPath } = useRouter();
+  const [lineStyle, setLineStyle] = React.useState<React.CSSProperties>({
+    bottom: 15,
+    transition: 'left 0.2s ease, width 0.2s ease',
+  });
+  const [w] = useWindowSize();
+
+  React.useEffect(() => {
+    const ele = document.getElementById(asPath);
+    setLineStyle((prev) => ({
+      ...prev,
+      left: ele?.offsetLeft,
+      width: ele?.clientWidth,
+    }));
+  }, [asPath, w]);
+
   return (
     <header>
       <div className="py-2.5 text-sm bg-black text-white">
         <div className="container flex">
           <p>Free shipping, 30 days return or refund guarantee.</p>
           <div className="flex-grow"></div>
-          <ul className="flex items-center gap-5 uppercase">
-            <li>sign in</li>
+          <ul className="flex items-center gap-6 uppercase tracking-widest">
+            <li>
+              {user ? (
+                <React.Fragment>
+                  <span className="normal-case">Hi</span>,{' '}
+                  <Link href={`/${user.email}`}>{user.name}</Link>
+                </React.Fragment>
+              ) : (
+                <Link href="/api/auth/login">sign in</Link>
+              )}
+            </li>
             <li>faqs</li>
             <li>usd</li>
           </ul>
@@ -24,24 +73,15 @@ const MainLayoutHeader: React.FC<{}> = () => {
       <div className="bg-white">
         <div className="container flex items-center">
           <h1 className="flex-1">Dotfashion</h1>
-          <ul className="flex-1 flex items-center justify-evenly py-6 gap-2">
-            <li>
-              <StyledLink href="#">Home</StyledLink>
-            </li>
-            <li>
-              <Link href="#">Shop</Link>
-            </li>
-            <li>
-              <Link href="#">Pages</Link>
-            </li>
-            <li>
-              <Link href="#">Blog</Link>
-            </li>
-            <li>
-              <Link href="#">Contacts</Link>
-            </li>
+          <ul className="relative flex-1 flex items-center justify-evenly py-6 gap-10">
+            {routes.map((r) => (
+              <li key={r.href} id={r.href}>
+                <StyledLink href={r.href}>{r.label}</StyledLink>
+              </li>
+            ))}
+            <li className="absolute h-[2px] bg-red-500" style={lineStyle} />
           </ul>
-          <div className="flex-1 flex text-xl gap-3 justify-end">
+          <div className="flex-1 flex text-xl gap-5 justify-end">
             <RiSearch2Line />
             <RiHeart2Line />
             <RiShoppingBagLine />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 import { AppPropsWithLayout } from './page';
 
 function Application({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return <UserProvider>{getLayout(<Component {...pageProps} />)}</UserProvider>;
 }
 
 export default Application;
